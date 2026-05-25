8df89a0a7fbef4f9709c3ba729d049e34cd12a36
Nathan <nathannxx@proton.me>
2026-05-26 02:56:16 +0700

    tanzanite: Change `FOX_VARIANT`
    Change-Id: Ia682a8c10d7c54c43a006ca91cbc1390ddd52227
    Signed-off-by: Nathan <nathannxx@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,7 +20,7 @@
 #
 
 export LC_ALL="C.UTF-8"
-export FOX_VARIANT="A12+_AOSP"
+export FOX_VARIANT="K5.10"
 export FOX_MAINTAINER_PATCH_VERSION=$(date +"%Y%m%d")
 export FOX_AB_DEVICE=1
 export FOX_VIRTUAL_AB_DEVICE=1
